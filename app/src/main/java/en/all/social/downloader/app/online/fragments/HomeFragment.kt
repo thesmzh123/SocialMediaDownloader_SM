fd01219d6568bee5d83e7c4c04345524e5ae5241
HP <thegame.mzh@gmail.com>
2020-09-11 16:07:03 +0500

    Initial commit

--- app/src/main/java/en/all/social/downloader/app/online/fragments/HomeFragment.kt
+++ app/src/main/java/en/all/social/downloader/app/online/fragments/HomeFragment.kt
@@ -14,6 +14,7 @@ import com.github.javiersantos.materialstyleddialogs.enums.Style
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import en.all.social.downloader.app.online.R
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.native_ad_layout.view.*
 
 
 @Suppress("UNUSED_ANONYMOUS_PARAMETER")
